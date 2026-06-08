cb816a6c128beb0dbbffc9de90f4ba1c0b6ab6c4
Smpcode253 <seanpooley434@gmail.com>
2026-06-08 12:28:19 -0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Autonomous Affiliate Engine</h1>
+      <p>Frontend is running.</p>
+    </div>
+  );
+}
